323a651486ae8573e0b8385a8655e9f02c0a41fc
Grayson Hicks <graysonhicks@gmail.com>
2021-12-23 01:00:16 -0500

    fix headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,9 @@ module.exports = {
       resolve: "gatsby-plugin-gatsby-cloud",
       options: {
         headers: {
-          "Access-Control-Allow-Origin:":
-            "https://connect.facebook.net/en_US/fbevents.js",
+          "/*": [
+            "Access-Control-Allow-Origin: https://connect.facebook.net/en_US/fbevents.js",
+          ],
         },
       },
     },
